4cd5e8bcb60e3b833c9115a8e0086ca741d5a77a
Karachev, Mikhail A <karachev.ma@parma.ru>
2019-09-02 18:32:10 +0500

    Lesson 1 First look on c#

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -8,9 +8,32 @@ namespace Lesson1
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void PrintHelloWorld()
         {
             Console.WriteLine("HelloWorld");
+        }
+        static void Main(string[] args)
+        {
+            PrintHelloWorld();
+            int x = new int();
+
+            Console.WriteLine("x = " + x);
+
+            int n = 6;
+            int [] array = new int[n];
+
+            for (int i=0;i<n;i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach(var num in array)
+            {
+                Console.WriteLine(num);
+            }
+
+            //ждём нажатия, чтобы не закрывать консоль
+            Console.ReadKey();
         }
     }
 }
